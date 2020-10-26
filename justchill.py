e6143df209818876d1eb2fd289db5614ef0d692c
Асылбек Тасбулатов (аиу-19 р/о) <71316760+Asylbek1337@users.noreply.github.com>
2020-10-26 08:48:05 +0300

    1

---
+++ justchill.py
@@ -0,0 +1,28 @@
+myString = 'myString value'
+myString2 = "myString2 value"
+
+myTuple = [100, 34, 46, 52, 34]
+myList = [13, 46, 79, 85, 95]
+print (myList)
+z = set (myList)
+print (z)
+x1 = 13
+x2 = 46
+x3 = 79
+x4 = 85
+x5 = 95
+
+y1 = 100
+y2 = 34
+y3 = 46
+y4 = 52
+y5 = 34
+
+print (x1 * y1)
+print (x2 * y2)
+print (x3 * y3)
+print (x4 * y4)
+print (x5 * y5)
+
+x = 13, 46, 79, 85, 95
+y = 100, 34, 46, 52, 34
